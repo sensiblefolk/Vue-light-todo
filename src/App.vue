9da0c76885ff20dd221af75fb1ece44a9afd4c93
michael anyi <mikeyeucresta@gmail.com>
2020-05-05 10:07:42 +0100

    vue todo app light iteration

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos v-bind:todos="todos" v-on:delete-todo="deleteTodo" />
+    <AddTodo v-on:add-todo="addTodo"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Learn Go',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Implement Python backend',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Cook food',
+          completed: false
+        },
+        {
+          id: 4,
+          title: 'Push to production',
+          completed: false
+        },
+        {
+          i: 5,
+          title: 'Build CI/CD Pipeline',
+          completed: false
+        }
+      ]
+    }
+  },
+  methods: {
+    addTodo(newTodoObj) {
+      this.todos = [...this.todos, newTodoObj]
+    },
+    deleteTodo(todoId) {
+      this.todos = this.todos.filter(todo => todo.id !== todoId);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
